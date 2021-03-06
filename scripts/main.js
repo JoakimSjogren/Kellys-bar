c2452f59332782190b6ee6533af5eee479250f3f
JoakimSjogren <70525815+JoakimSjogren@users.noreply.github.com>
2021-03-06 14:11:55 +0100

    Small changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,9 @@
-const slider = document.querySelector('.slider-container');
-const sliderImages = document.querySelectorAll('.slider-container img');
+const userText = document.querySelector('.user-text');
 
-const nxtBtn = document.querySelector('.next-btn');
-const prevBtn = document.querySelector('.prev-btn');
 
-let counter = 1;
-let size = sliderImages[0].clientWidth;
-
-window.addEventListener('resize', () => {
-    size = sliderImages[0].clientWidth;
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-nxtBtn.addEventListener('click', () => {
-    if (counter >= sliderImages.length -1)
-        return;
-    counter++;
-    slide();
-});
-
-prevBtn.addEventListener('click', () => {
-    if (counter <= 0)
-        return;
-    counter--;
-    slide();
-    
-});
-
-function slide() {
-    slider.style.transition = "transform 0.6s ease-in-out";
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-}
-
-slider.addEventListener('transitionend', () => {
-    if (counter === 0) {
-        slider.style.transition = "none";
-        counter = sliderImages.length -2;
-        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }
-    else if (counter === sliderImages.length-1) {
-        slider.style.transition = "none";
-        counter = 1;
-        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }
-});
+const urlParams = new URLSearchParams(location.search);
+for (const [key, value] of urlParams) {
+    let name = value;
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+    userText.textContent += ` ${name}`;
+};
---
+++ scripts/slider.js
@@ -0,0 +1,48 @@
+const slider = document.querySelector('.slider-container');
+const sliderImages = document.querySelectorAll('.slider-container img');
+
+const nxtBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+let counter = 1;
+let size = sliderImages[0].clientWidth;
+
+window.addEventListener('resize', () => {
+    size = sliderImages[0].clientWidth;
+    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nxtBtn.addEventListener('click', () => {
+    if (counter >= sliderImages.length -1)
+        return;
+    counter++;
+    slide();
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0)
+        return;
+    counter--;
+    slide();
+    
+});
+
+function slide() {
+    slider.style.transition = "transform 0.6s ease-in-out";
+    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+}
+
+slider.addEventListener('transitionend', () => {
+    if (counter === 0) {
+        slider.style.transition = "none";
+        counter = sliderImages.length -2;
+        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    else if (counter === sliderImages.length-1) {
+        slider.style.transition = "none";
+        counter = 1;
+        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
