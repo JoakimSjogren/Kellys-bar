e29f9bfdde2014580cc04342516c0fa47cffb6ff
JoakimSjogren <70525815+JoakimSjogren@users.noreply.github.com>
2021-03-17 13:00:48 +0100

    Small changes

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const userText = document.querySelector('.user-text');
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const firstNameInput = document.querySelector('#firstname');
+const lastNameInput = document.querySelector('#lastname');
 
 const urlParams = new URLSearchParams(location.search);
 for (const [key, value] of urlParams) {
@@ -11,8 +12,10 @@ for (const [key, value] of urlParams) {
     let user = users[Object.keys(users)[id -1]];
     if (user !== undefined) {
         let firstName = user.firstName;
+        let lastName = user.lastName;
         userText.textContent += " " + firstName;
         firstNameInput.value = firstName;
+        lastNameInput.value = lastName;
     }
 };
 
--- scripts/slider.js
+++ scripts/slider.js
@@ -9,10 +9,10 @@ let size = sliderImages[0].clientWidth;
 
 window.addEventListener('resize', () => {
     size = sliderImages[0].clientWidth;
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    slider.style.transform = 'translateX(' + -(size * counter) + 'px)';
 });
 
-slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+slider.style.transform = 'translateX(' + -(size * counter) + 'px)';
 
 nxtBtn.addEventListener('click', () => {
     if (counter >= sliderImages.length -1)
@@ -30,19 +30,19 @@ prevBtn.addEventListener('click', () => {
 
 function slide() {
     slider.style.transition = "transform 0.6s ease-in-out";
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    slider.style.transform = 'translateX(' + -(size * counter) + 'px)';
 }
 
 slider.addEventListener('transitionend', () => {
     if (counter === 0) {
         slider.style.transition = "none";
         counter = sliderImages.length -2;
-        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        slider.style.transform = 'translateX(' + -(size * counter) + 'px)';
     }
     else if (counter === sliderImages.length-1) {
         slider.style.transition = "none";
         counter = 1;
-        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        slider.style.transform = 'translateX(' + -(size * counter) + 'px)';
     }
 });
 
